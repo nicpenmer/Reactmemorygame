b41dc96ab7fe34a67984f9e852a29e40b78d3b87
nicpenmer <nicole.penmer@gmail.com>
2018-06-13 18:36:34 -0400

    updates

--- reactice/src/components/Card/Card.js
+++ reactice/src/components/Card/Card.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./Card.css";
 
 const Card = props => (
-  <div className="card" onClick={() => props.clickCount(props.id)}>
-    <div className="img-container">
-      <img alt={props.name} src={props.image} />
-    </div>
+	<div onClick={() => props.setClicked(props.id)} className="card">
+		<div className="img-container">
+      		<img alt={props.name} src={props.image} />
+    	</div>
   </div>
 );
-
 export default Card;
+
+
